4657d089075ddb4bba387a6afa3ebc3096753f28
greinisch <greinisch@youwol.com>
2021-12-28 18:14:23 +0100

    :sparkles: user-settings.client (mocked using local storage)

--- src/lib/clients/user-settings/user-settings.client.ts
+++ src/lib/clients/user-settings/user-settings.client.ts
@@ -1,6 +1,6 @@
-import { Observable } from 'rxjs';
-import { RequestOptions, resolveRequest } from '../utils';
-
+import { Observable, of, ReplaySubject } from 'rxjs';
+import { Json, RequestOptions, resolveRequest } from '../utils';
+import { cloneDeep, mergeWith } from 'lodash'
 
 
 export class UserSettingsClient {
@@ -32,18 +32,67 @@ export class UserSettingsClient {
         return new Headers({ ...this.headers, ...headers })
     }
 
+    static localStorageKey = "user-settings"
+
+    static getData(): Json {
+
+        if (!localStorage.getItem(UserSettingsClient.localStorageKey))
+            localStorage.setItem(UserSettingsClient.localStorageKey, "{}")
+
+        return JSON.parse(localStorage.getItem(UserSettingsClient.localStorageKey))
+    }
+
+    static saveData(d: Json) {
+        localStorage.setItem(UserSettingsClient.localStorageKey, JSON.stringify(d))
+    }
+
+    settings$: { [key: string]: ReplaySubject<unknown> } = {}
+    defaultSettings: { [key: string]: Json } = {}
+
     /**
      * 
      * @param applicationId id of the application
+     * @param defaultSettings default settings of the application
      * @param options request options
      * @returns response
      */
-    querySettings(applicationId: string, options: RequestOptions = {}): Observable<unknown> {
+    querySettings<T>(applicationId: string, defaultSettings: T, options: RequestOptions = {}): Observable<T> {
 
-        let request = new Request(
-            `${this.basePath}/healthz`,
-            { method: 'GET', headers: this.getHeaders(options.headers) }
-        );
-        return resolveRequest(request, 'query', { requestId: "healthz", ...options })
+        this.defaultSettings[applicationId] = defaultSettings as unknown as Json
+
+        this.emitSettings(applicationId)
+        return this.settings$[applicationId] as ReplaySubject<T>
+    }
+
+    /**
+     * 
+     * @param applicationId id of the application
+     * @param defaultSettings default settings of the application
+     * @param options request options
+     * @returns response
+     */
+    updateSettings<T>(applicationId: string, settingsUpdate: T, options: RequestOptions = {}): Observable<T> {
+
+        let data = UserSettingsClient.getData()
+        data[applicationId] = settingsUpdate
+        UserSettingsClient.saveData(data)
+
+        if (!this.defaultSettings[applicationId])
+            return
+
+        this.emitSettings(applicationId)
+    }
+
+    emitSettings(applicationId: string) {
+
+        if (!this.settings$[applicationId])
+            this.settings$[applicationId] = new ReplaySubject<Json>(1)
+
+        let newAttributes = UserSettingsClient.getData()?.[applicationId]
+
+        let merged = cloneDeep(this.defaultSettings[applicationId])
+        mergeWith(merged, newAttributes)
+
+        this.settings$[applicationId].next(merged)
     }
 }
