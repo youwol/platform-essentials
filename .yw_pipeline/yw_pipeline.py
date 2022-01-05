50ba7c17c0beeb4af238b4241bbc6b6b0af93355
greinisch <greinisch@youwol.com>
2022-01-05 17:43:39 +0100

    :wrench: update yw_pipeline.py

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,8 +1,5 @@
-from youwol.configuration import Pipeline
-from youwol.configuration import UserConfiguration
-
-def pipeline(configuration: UserConfiguration) -> Pipeline:
-
-    return configuration.get_custom_pipeline("typescript-webpack-npm")
+import youwol.pipelines.pipeline_typescript_weback_npm
 
 
+def pipeline(configuration):
+    return youwol.pipelines.pipeline_typescript_weback_npm.pipeline()
