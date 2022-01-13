a127ca3c4e84aaa95bcbc3261f37d5f56221d8c3
greinisch <greinisch@youwol.com>
2022-01-13 18:19:58 +0100

    :alien: sync yw_pipeline

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,5 +1,11 @@
 import youwol.pipelines.pipeline_typescript_weback_npm
+from youwol.environment.models import IPipelineFactory
 
 
-def pipeline(configuration):
-    return youwol.pipelines.pipeline_typescript_weback_npm.pipeline()
+class PipelineFactory(IPipelineFactory):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    async def get(self):
+        return youwol.pipelines.pipeline_typescript_weback_npm.pipeline()
