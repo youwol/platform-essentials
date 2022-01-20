6b5bb2d745c120528779ed769de414143f3597af
greinisch <greinisch@youwol.com>
2022-01-20 17:29:17 +0100

    ✅ :sparkles: explorer => add test delete-trash-purge

---
+++ src/tests/explorer/delete-trash-purge.test.ts
@@ -0,0 +1,53 @@
+import '../mock-requests'
+import { AssetsGatewayClient } from '../../lib/clients/assets-gateway/assets-gateway.client'
+
+import { getPyYouwolBasePath, resetPyYouwolDbs } from '../common'
+
+AssetsGatewayClient.staticBasePath = `${getPyYouwolBasePath()}/api/assets-gateway`
+
+import { cd, deleteDrive, mkDir, mkFluxApp, purgeTrash, rm, selectItem, shell$ } from './shell'
+
+
+beforeEach(async (done) => {
+
+    jest.setTimeout(900 * 1000)
+    resetPyYouwolDbs().then(() => {
+        done()
+    })
+})
+
+
+test('delete drive', (done) => {
+
+    shell$().pipe(
+        cd('..'),
+        cd('..'),
+        selectItem('Default drive'),
+        deleteDrive()
+    ).subscribe(() => {
+        done()
+    })
+})
+
+
+test('Create, delete, clear trash & purge drive', (done) => {
+
+    let projectName = 'my flux-app'
+    shell$().pipe(
+        mkDir("test folder"),
+        cd('test folder'),
+        mkFluxApp(projectName),
+        cd('..'),
+        rm("test folder"),
+        cd('..'),
+        cd('Trash'),
+        purgeTrash(),
+        cd('..'),
+        cd('..'),
+        selectItem('Default drive'),
+        deleteDrive()
+    ).subscribe(() => {
+        done()
+    })
+})
+
