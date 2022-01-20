17330f9ba80139230ff10780192b6d2993799724
greinisch <greinisch@youwol.com>
2022-01-20 15:20:39 +0100

    ✅ ♻️ explorer tests use 'Shell' helper

--- src/tests/explorer/navigation.test.ts
+++ src/tests/explorer/navigation.test.ts
@@ -1,230 +1,77 @@
 import '../mock-requests'
 import { AssetsGatewayClient } from '../../lib/clients/assets-gateway/assets-gateway.client'
 
-import { expectAttributes, getFromDocument, getPyYouwolBasePath, queryFromDocument, resetPyYouwolDbs } from '../common'
+import { getPyYouwolBasePath, resetPyYouwolDbs } from '../common'
 
 AssetsGatewayClient.staticBasePath = `${getPyYouwolBasePath()}/api/assets-gateway`
-
-import { ExplorerState, FolderContentView, HeaderPathView, MainPanelView } from '../../lib'
-import { render } from '@youwol/flux-view'
-import { combineLatest } from 'rxjs'
-import { ItemView } from '../../lib/explorer/views/main-panel/folder-content/item.view'
-import { BrowserNode, DriveNode, FolderNode, FutureNode, GroupNode } from '../../lib/explorer/nodes'
-import { mergeMap, skip, skipWhile, take } from 'rxjs/operators'
-import { RowView } from '../../lib/explorer/views/main-panel/folder-content/details.view'
-import { ActionsMenuView, PathElementView } from '../../lib/explorer/views/main-panel/header-path.view'
-import { ActionBtnView, ActionsView } from '../../lib/explorer/views/main-panel/actions.view'
-import { selectAndApply$ } from './common'
+import { BrowserNode, FolderNode } from '../../lib/explorer/nodes'
+import { cd, expectSnapshot, mkDir, rm, selectItem, shell$ } from './shell'
 
 
-beforeAll(async (done) => {
+let FolderSelectedActions = [
+    "new folder",
+    "new app",
+    "new story",
+    "paste",
+    "import data",
+    "refresh",
+    "rename",
+    "delete",
+    "cut",
+    "refresh",
+]
+
+beforeEach(async (done) => {
     resetPyYouwolDbs().then(() => {
         done()
     })
 })
 
-let state: ExplorerState
+test('navigation basics', (done) => {
 
+    let newFolderName = 'new folder'
+    let expectMyFolder = (item: BrowserNode) => {
+        expect(item).toBeInstanceOf(FolderNode)
+        expect(item.name).toEqual(newFolderName)
+    }
 
-test('explorer state initialization', (done) => {
-
-    state = new ExplorerState()
-
-    combineLatest([
-        state.userInfo$,
-        state.userDrives$,
-        state.defaultUserDrive$
-    ]).subscribe(([userInfo, drives, defaults]) => {
-        expect(userInfo.name).toEqual("int_tests_yw-users@test-user")
-        expect(drives.length).toEqual(0)
-        expectAttributes(defaults, [
-            'driveId',
-            'driveName',
-            'groupId',
-            'homeFolderId',
-            'homeFolderName',
-            'downloadFolderId',
-            'downloadFolderName',
-            'systemFolderId',
-            'systemFolderName',
-            'systemPackagesFolderId',
-            'systemPackagesFolderName',
-            'desktopFolderId',
-            'desktopFolderName',
-        ])
-    })
-    state.currentFolder$
-        .pipe(take(1))
-        .subscribe(({ folder }) => {
-            expect(folder.name).toEqual('Home')
-            done()
-        })
-})
-
-test('explorer view initialization', (done) => {
-
-    document.body.appendChild(render({
-        children: [
-            new HeaderPathView({
-                state
-            }),
-            new MainPanelView({
-                state
-            })
-        ]
-    }))
-
-    let headerView = getFromDocument<HeaderPathView>(`.${HeaderPathView.ClassSelector}`)
-    expect(headerView).toBeTruthy()
-
-    let mainView = getFromDocument<MainPanelView>(`.${MainPanelView.ClassSelector}`)
-    expect(mainView).toBeTruthy()
-
-    let folderContentView = getFromDocument<FolderContentView>(`.${FolderContentView.ClassSelector}`)
-    expect(folderContentView).toBeTruthy()
-
-    let items = queryFromDocument<ItemView>(`.${ItemView.ClassSelector}`)
-    expect(items.length).toEqual(0)
-
-    let pathElements = queryFromDocument<PathElementView>(`.${PathElementView.ClassSelector}`)
-    expect(pathElements[0].node).toBeInstanceOf(GroupNode)
-    expect(pathElements[1].node).toBeInstanceOf(DriveNode)
-    expect(pathElements[2].node).toBeInstanceOf(FolderNode)
-    expect(pathElements[2].node.kind).toEqual("home")
-
-    let actionsMenuView = getFromDocument<ActionsMenuView>(`.${ActionsMenuView.ClassSelector}`)
-    expect(actionsMenuView).toBeTruthy()
-    actionsMenuView.dispatchEvent(new MouseEvent('click', { bubbles: true }))
-
-    let actionsContainer = getFromDocument<ActionsView>(`.${ActionsView.ClassSelector}`)
-    expect(actionsContainer).toBeTruthy()
-
-    actionsContainer.displayedActions$
-        .pipe(take(1))
-        .subscribe(({ item, folder, actions }) => {
-
-            let actionsView = queryFromDocument<ActionBtnView>(`.${ActionBtnView.ClassSelector}`)
-            let actionNames = actionsView.map(a => a.action.name)
-
-            expect(actionNames).toEqual([
-                "new folder",
-                "new app",
-                "new story",
-                "paste",
-                "import data",
-                "refresh",
-            ])
-            done()
-        })
-})
-
-test('create a folder', (done) => {
-
-    let actionNewFolder = getFromDocument<ActionBtnView>(
-        `.${ActionBtnView.ClassSelector}`,
-        (view) => view.action.name == "new folder"
-    )
-    expect(actionNewFolder).toBeTruthy()
-
-    actionNewFolder.dispatchEvent(new MouseEvent('click', { bubbles: true }))
-    let folderContentView = getFromDocument<FolderContentView>(`.${FolderContentView.ClassSelector}`)
-
-    folderContentView.items$.pipe(
-        take(1)
-    ).subscribe((items) => {
-        expect(items.length).toEqual(1)
-        expect(items[0]).toBeInstanceOf(FutureNode)
-    })
-
-    folderContentView.items$.pipe(
-        skip(1),
-        take(1)
-    ).subscribe((items) => {
-
-        expect(items.length).toEqual(1)
-        expect(items[0]).toBeInstanceOf(FolderNode)
-
-        let itemsView = queryFromDocument<RowView>(`.${RowView.ClassSelector}`)
-        expect(itemsView.length).toEqual(1)
-        expect(itemsView[0].item.name).toEqual('new folder')
-        done()
-    })
-})
-
-test('select folder', (done) => {
-
-    let rowView = getFromDocument<RowView>(
-        `.${RowView.ClassSelector}`,
-        (row) => row.item.name == 'new folder'
-    )
-    expect(rowView).toBeTruthy()
-
-    rowView.dispatchEvent(new MouseEvent('click', { bubbles: true }))
-
-    state.selectedItem$.pipe(
-        take(1)
-    ).subscribe((folder) => {
-        expect(folder.id).toEqual(rowView.item.id)
-        done()
-    })
-})
-
-test('open folder', (done) => {
-
-    let rowView = getFromDocument<RowView>(
-        `.${RowView.ClassSelector}`,
-        (row) => row.item.name == 'new folder'
-    )
-    expect(rowView).toBeTruthy()
-
-    rowView.dispatchEvent(new MouseEvent('dblclick', { bubbles: true }))
-
-    state.currentFolder$.subscribe(({ folder }) => {
-        expect(folder.name).toEqual('new folder')
-        let folderContentView = getFromDocument<FolderContentView>(`.${FolderContentView.ClassSelector}`)
-        expect(folderContentView.folderId).toEqual(folder.id)
-    })
-
-    // Wait for actions menu view to be updated
-    state.currentFolder$.pipe(
-        mergeMap(() => {
-            let actionsContainer = getFromDocument<ActionsView>(`.${ActionsView.ClassSelector}`)
-            return actionsContainer.displayedActions$
+    shell$().pipe(
+        mkDir(newFolderName),
+        expectSnapshot({
+            items: (items) => {
+                expect(items.length).toEqual(1)
+                expectMyFolder(items[0])
+            }
         }),
-        skipWhile(({ folder }) => folder.name != 'new folder'),
-        take(1)
-    ).subscribe((actions) => {
-        let actionBtnsView = queryFromDocument<ActionBtnView>(`.${ActionBtnView.ClassSelector}`)
-        actionBtnsView.forEach(node => {
-            expect(node.action.sourceEventNode.name).toEqual('new folder')
+        selectItem(newFolderName),
+        expectSnapshot({
+            explorerState: (state) => {
+                expect(state.selectedItem$.getValue().name).toEqual(newFolderName)
+            },
+            actions: (actions) => {
+                expect(actions.map(a => a.name)).toEqual(FolderSelectedActions)
+            }
+        }),
+        cd(newFolderName),
+        expectSnapshot({
+            items: (items) => {
+                expect(items.length).toEqual(0)
+            }
+        }),
+        cd('..'),
+        expectSnapshot({
+            items: (items) => {
+                expect(items.length).toEqual(1)
+                expectMyFolder(items[0])
+            }
+        }),
+        rm(newFolderName),
+        expectSnapshot({
+            items: (items) => {
+                expect(items.length).toEqual(0)
+            }
         })
+    ).subscribe(() => {
         done()
     })
 })
-
-
-test('navigate back', (done) => {
-
-    let pathElements = queryFromDocument<PathElementView>(`.${PathElementView.ClassSelector}`)
-    expect(pathElements.length).toEqual(4)
-    expect(pathElements[3].node.name).toEqual("new folder")
-    pathElements[2].dispatchEvent(new MouseEvent('click', { bubbles: true }))
-
-    state.currentFolder$.subscribe(({ folder }) => {
-
-        let pathElements = queryFromDocument<PathElementView>(`.${PathElementView.ClassSelector}`)
-        expect(pathElements.length).toEqual(3)
-        done()
-    })
-})
-
-
-test('delete folder', (done) => {
-
-    selectAndApply$('new folder', 'delete')
-        .subscribe((items: BrowserNode[]) => {
-            expect(items.length).toEqual(0)
-            done()
-        })
-})
-
--- src/tests/explorer/story-creation.test.ts
+++ src/tests/explorer/story-creation.test.ts
@@ -1,143 +1,56 @@
 import '../mock-requests'
 import { AssetsGatewayClient } from '../../lib/clients/assets-gateway/assets-gateway.client'
 
-import { getFromDocument, getPyYouwolBasePath, resetPyYouwolDbs } from '../common'
+import { getPyYouwolBasePath, resetPyYouwolDbs } from '../common'
 
 AssetsGatewayClient.staticBasePath = `${getPyYouwolBasePath()}/api/assets-gateway`
-
-import { AssetCardView, ExplorerState, FolderContentView, HeaderPathView, MainPanelView } from '../../lib'
-import { render } from '@youwol/flux-view'
-import { InfoBtnView, ItemView } from '../../lib/explorer/views/main-panel/folder-content/item.view'
-import { FutureNode, StoryNode } from '../../lib/explorer/nodes'
-import { filter, skip, take } from 'rxjs/operators'
-import { RowView } from '../../lib/explorer/views/main-panel/folder-content/details.view'
-import { ActionBtnView, ActionsView } from '../../lib/explorer/views/main-panel/actions.view'
+import { StoryNode } from '../../lib/explorer/nodes'
+import { expectSnapshot, mkStory, popupInfo, rm, selectItem, shell$ } from './shell'
 
 
-beforeAll(async (done) => {
+beforeEach(async (done) => {
     resetPyYouwolDbs().then(() => {
         done()
     })
 })
 
-let state: ExplorerState
 
+test('Create, info & delete flux application', (done) => {
 
-test('explorer state initialization', (done) => {
-
-    state = new ExplorerState()
-
-    state.currentFolder$
-        .pipe(take(1))
-        .subscribe(({ folder }) => {
-            expect(folder.name).toEqual('Home')
-            done()
-        })
-})
-
-test('explorer view initialization', (done) => {
-
-    document.body.appendChild(render({
-        children: [
-            new HeaderPathView({
-                state
-            }),
-            new MainPanelView({
-                state
-            })
-        ]
-    }))
-
-    let actionsContainer = getFromDocument<ActionsView>(`.${ActionsView.ClassSelector}`)
-    expect(actionsContainer).toBeTruthy()
-
-    actionsContainer.displayedActions$
-        .pipe(take(1))
-        .subscribe(() => {
-            done()
-        })
-})
-
-
-test('create story', (done) => {
-
-    let actionNewStory = getFromDocument<ActionBtnView>(
-        `.${ActionBtnView.ClassSelector}`,
-        (view) => view.action.name == "new story"
-    )
-    expect(actionNewStory).toBeTruthy()
-
-    actionNewStory.dispatchEvent(new MouseEvent('click', { bubbles: true }))
-
-    let folderContentView = getFromDocument<FolderContentView>(`.${FolderContentView.ClassSelector}`)
-
-    folderContentView.items$.pipe(
-        take(1)
-    ).subscribe((items) => {
-        expect(items.length).toEqual(1)
-        expect(items[0]).toBeInstanceOf(FutureNode)
-        expect(items[0].name).toEqual('new story')
-    })
-
-    folderContentView.items$.pipe(
-        skip(1),
-        take(1)
-    ).subscribe((items) => {
-
-        expect(items.length).toEqual(1)
-        let storyNode = items[0] as StoryNode
-        expect(storyNode.kind).toEqual('story')
-        expect(storyNode.name).toEqual('new story')
-        done()
-    })
-})
-
-
-test('select story', (done) => {
-
-    let itemView = getFromDocument<ItemView>(
-        `.${ItemView.ClassSelector}`,
-        (view) => view.item.name == "new story"
-    )
-    expect(itemView).toBeTruthy()
-
-    itemView.dispatchEvent(new MouseEvent('click', { bubbles: true }))
-
-    state.selectedItem$.pipe(
-        take(1)
-    ).subscribe((item) => {
-        expect(item.name).toEqual('new story')
-        done()
-    })
-})
-
-test('popup info view', (done) => {
-
-    let rowView = getFromDocument<RowView>(
-        `.${RowView.ClassSelector}`,
-        (row) => row.item.name == 'new story'
-    )
-    expect(rowView).toBeTruthy()
-    rowView.onmouseenter()
-
-    let infoBtn = getFromDocument<InfoBtnView>(
-        `.${InfoBtnView.ClassSelector}`,
-        (infoView) => infoView.node.name == 'new story'
-    )
-    expect(infoBtn).toBeTruthy()
-
-    infoBtn.dispatchEvent(new MouseEvent('click', { bubbles: true }))
-
-    infoBtn.popupDisplayed$.pipe(
-        filter((isDisplayed) => isDisplayed)
+    let storyName = 'my story'
+    shell$().pipe(
+        mkStory(storyName),
+        expectSnapshot(
+            {
+                items: (items) => {
+                    expect(items.length).toEqual(1)
+                    let fluxNode = items[0] as StoryNode
+                    expect(fluxNode.kind).toEqual('story')
+                    expect(fluxNode.name).toEqual(storyName)
+                }
+            }
+        ),
+        selectItem(storyName),
+        popupInfo(),
+        expectSnapshot(
+            {
+                assetCardView: (assetCardView) => {
+                    expect(assetCardView).toBeTruthy()
+                    expect(assetCardView.withTabs.Permissions).toBeTruthy()
+                    expect(assetCardView.asset.kind).toEqual('story')
+                    expect(assetCardView.asset.name).toEqual(storyName)
+                }
+            }
+        ),
+        rm(storyName),
+        expectSnapshot(
+            {
+                items: (items) => {
+                    expect(items.length).toEqual(0)
+                }
+            }
+        ),
     ).subscribe(() => {
-        let assetCardView = getFromDocument<AssetCardView>(
-            `.${AssetCardView.ClassSelector}`
-        )
-        expect(assetCardView).toBeTruthy()
-        expect(assetCardView.withTabs.Permissions).toBeTruthy()
-        expect(assetCardView.asset.kind).toEqual('story')
-        expect(assetCardView.asset.name).toEqual('new story')
         done()
     })
 })
