61f814c9bd0f926b738c26914d0ff2a4c33fe4c6
greinisch <greinisch@youwol.com>
2022-03-25 11:44:35 +0100

    🔧 webpack.config => fix typo in external fv-input

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
             '@youwol/fv-button': '@youwol/fv-button',
             '@youwol/fv-tree': '@youwol/fv-tree',
             '@youwol/fv-tabs': '@youwol/fv-tabs',
-            '@youwol/fv-inputs': '@youwol/fv-inputs',
+            '@youwol/fv-input': '@youwol/fv-input',
             '@youwol/http-clients': '@youwol/http-clients',
             'js-beautify': 'js_beautify',
             lodash: '_',
