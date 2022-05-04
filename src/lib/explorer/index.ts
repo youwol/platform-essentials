99fedfb5910b02fec1160bd1673eb9d26241a3af
greinisch <greinisch@youwol.com>
2022-05-04 16:48:28 +0200

    ✨ explorer => expose TreeGroup, Action & getActions$

--- src/lib/explorer/index.ts
+++ src/lib/explorer/index.ts
@@ -1,5 +1,7 @@
-export { ExplorerState } from './explorer.state'
+export { ExplorerState, TreeGroup } from './explorer.state'
 export { SideBarView } from './views/sidebar/sidebar.view'
 export { MainPanelView } from './views/main-panel/main-panel.view'
 export { HeaderPathView } from './views/main-panel/header-path.view'
 export { FolderContentView } from './views/main-panel/folder-content/folder-content.view'
+export { Action, getActions$ } from './actions.factory'
+export * from './nodes'
