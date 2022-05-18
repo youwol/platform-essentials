17b7c8e685c8a074adceef4f721c8c64e927d355
greinisch <greinisch@youwol.com>
2022-05-18 09:24:58 +0200

    ♻️ Explorer => rework MainPanel view (split items list & asset-view)

--- src/lib/explorer/views/main-panel/main-panel.view.ts
+++ src/lib/explorer/views/main-panel/main-panel.view.ts
@@ -1,10 +1,13 @@
-import { child$, HTMLElement$, VirtualDOM } from '@youwol/flux-view'
-import { FolderContentView } from '../..'
+import { attr$, child$, VirtualDOM } from '@youwol/flux-view'
+import { AnyItemNode, FolderContentView } from '../..'
 import { ExplorerState } from '../../explorer.state'
+import { filter, mergeMap } from 'rxjs/operators'
+import { AssetsGateway, raiseHTTPErrors } from '@youwol/http-clients'
+import { AssetView } from '../../../assets'
 
 export class MainPanelView implements VirtualDOM {
     static ClassSelector = 'main-panel-view'
-    public readonly class = `${MainPanelView.ClassSelector} w-100 h-100 flex-grow-1 d-flex flex-column`
+    public readonly class = `${MainPanelView.ClassSelector} w-100 h-100 d-flex`
     public readonly style = {
         minHeight: '0px',
     }
@@ -17,26 +20,50 @@ export class MainPanelView implements VirtualDOM {
 
         this.children = [
             {
-                class: 'flex-grow-1 w-100 d-flex',
-                style: { minHeight: '0px' },
-                connectedCallback: (elem: HTMLElement$) => {
-                    elem.ownSubscriptions(...this.state.subscriptions)
+                class: attr$(
+                    this.state.selectedItem$,
+                    (item): string => (item ? 'w-25' : 'w-50'),
+                    { wrapper: (d) => `${d} h-100` },
+                ),
+                style: {
+                    minWidth: '250px',
+                    maxWidth: '400px',
                 },
                 children: [
-                    {
-                        class: 'w-100 h-100 d-flex',
-                        children: [
-                            child$(this.state.openFolder$, ({ folder }) => {
-                                return new FolderContentView({
-                                    state: this.state,
-                                    folderId: folder.id,
-                                    groupId: folder.groupId,
-                                })
-                            }),
-                        ],
-                    },
+                    child$(this.state.openFolder$, ({ folder }) => {
+                        return new FolderContentView({
+                            state: this.state,
+                            folderId: folder.id,
+                            groupId: folder.groupId,
+                        })
+                    }),
                 ],
-                onclick: () => this.state.selectedItem$.next(undefined),
+            },
+            {
+                class: attr$(this.state.selectedItem$, (item): string =>
+                    item ? 'd-block w-75' : 'd-none',
+                ),
+                style: {
+                    boxShadow: 'white 0px 0px 5px',
+                },
+                children: [
+                    child$(
+                        this.state.selectedItem$.pipe(
+                            filter((d) => d != undefined),
+                            mergeMap((node: AnyItemNode) =>
+                                new AssetsGateway.Client().assets.getAsset$({
+                                    assetId: node.assetId,
+                                }),
+                            ),
+                            raiseHTTPErrors(),
+                        ),
+                        (asset) => {
+                            return new AssetView({
+                                asset,
+                            })
+                        },
+                    ),
+                ],
             },
         ]
     }
