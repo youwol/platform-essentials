bb3c0bae8de02e133b15c95d81c6977b7f867d93
greinisch <greinisch@youwol.com>
2022-05-18 09:15:23 +0200

    ✨ assets => add new asset.view

--- src/lib/assets/asset-card/index.ts
+++ src/lib/assets/asset-card/index.ts
@@ -6,7 +6,7 @@
  * @module lib/assets
  */
 
-export { AssetCardView } from './asset-card.view'
+export { AssetView } from './asset.view'
 export { AssetPermissionsView } from './permissions/permissions.view'
 export { AssetOverview } from './overview/overview.view'
 export { PackageInfoView } from './asset-specific/package-info.view'
